2c65c6a002aed6e8ff95e28796d8e4fb6ffbe504
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-12-11 12:15:28 -0500

    Add do now

---
+++ script.js
@@ -0,0 +1,11 @@
+let button = document.querySelector("button");
+
+button.addEventListener("click", function() {
+    let weather = document.querySelector(".weather").value;
+    
+    // 1. Write a conditional statement:
+    //  - If the input is "rainy", change the text in the paragraph tag to "Bring an umbrella!"
+    //  - Else, change the text in the paragraph tag to "Enjoy the sun!"
+
+
+});
