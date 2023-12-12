eacb334fe3e70bf15fe02bbad408173465a57fab
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-12-12 15:47:00 -0500

    Added coding practice & navbar

---
+++ conditionals.js
@@ -0,0 +1,25 @@
+let button = document.querySelector(".button");
+let californiaMessage = document.querySelector(".california");
+let illinoisMessage = document.querySelector(".illinois");
+let newyorkMessage = document.querySelector(".new-york");
+let other = document.querySelector(".other");
+
+button.addEventListener("click", function() {
+    let state = document.querySelector(".state").value;
+
+    // CODE ALONG
+    // 1. Write an if statement that would display the california message if a user says they're from "California".
+    
+    // CODE SOLO
+    // 2. Write an else if statement that would display the illinois message if a user says they're from "Illinois".
+    
+    // 3. Write an else if statement that would display the new york message if a user says they're from "New York".
+    
+    // 4. Write an else statement that displays the other message if the user types in a different state.
+
+
+});
+
+// Finished early? 
+//  - Add another state of your choosing!
+//  - Use compound conditionals so it accepts a user typing the state in different ways: all caps, all lowercase, and first letter capitalized.
